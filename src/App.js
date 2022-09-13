e820eb275a17c65f6077a7985c39cad813d06465
Vincent Ferraro <v.ferraro@live.fr>
2022-09-13 23:09:21 +0200

    add form balise

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import logo from './ouivalo-logo.png'
 import './App.css';
-
+import Form from './component/Form'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="App">
+      <img src={logo} alt="logo-ouivalo" />
+      <Form />
     </div>
+    
+      
+      
+      
+    
   );
 }
 
---
+++ src/component/Form.js
@@ -0,0 +1,25 @@
+import { Component } from 'react'
+import '../style/Form.css'
+
+class Form extends Component{
+
+
+    render() {
+        return  <div>
+            <form method='post'>
+        <label for='mail'>Mail: <input type="mail" /></label>
+        
+        <label for='date'>Date <input type="date"/></label>
+        
+        <label >URL <input type="text"/></label>
+        
+        <label>Message: <textarea /></label>
+        
+        <input type="submit" value="Envoyer" />
+        </form>
+        </div>
+        
+    }
+}
+
+export default Form
